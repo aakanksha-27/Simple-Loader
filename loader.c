d07e552f5c472bc983292d4ae698f5e5f4436e44
palak363 <163020319+palak363@users.noreply.github.com>
2024-08-30 19:44:39 +0530

    Update loader.c

--- loader.c
+++ loader.c
@@ -16,17 +16,41 @@ void loader_cleanup() {
  * Load and run the ELF executable file
  */
 void load_and_run_elf(char** exe) {
-  fd = open(exe[1], O_RDONLY);
   // 1. Load entire binary content into the memory from the ELF file.
+    fd = open(exe[1], O_RDONLY);
+    if (fd < 0) {
+        printf("Error in opening file");
+        exit (1);
+    }
+    off_t file_size = lseek(fd, 0, SEEK_END);
+    if (file_size < 0) {
+        printf("Error seeking to end of file");
+        close(fd);
+        exit (1);
+    }
+    
+    lseek(fd, 0, SEEK_SET);
+    char *buffer = (char *)malloc(file_size);
+
+    if (buffer == NULL) {
+        printf("Error during Memory allocation");
+        close(fd);
+        exit (1) ;
+    }
+    ssize_t file_read = read(fd, buffer, file_size);
+    close(fd);
+  
+    
   // 2. Iterate through the PHDR table and find the section of PT_LOAD
   //    type that contains the address of the entrypoint method in fib.c
   // 3. Allocate memory of the size "p_memsz" using mmap function
   //    and then copy the segment content
   // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
+    int (*_start)(void) = (int (*)(void))(ehdr->e_entry);
   // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
+    int result = _start();
   // 6. Call the "_start" method and print the value returned from the "_start"
-  int result = _start();
-  printf("User _start return value = %d\n",result);
+    printf("User _start return value = %d\n",result);
 }
 
 int main(int argc, char** argv)
