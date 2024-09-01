98661b3ba885ace2a0f1c183475281dd10a9258c
aakanksha-27 <163016647+aakanksha-27@users.noreply.github.com>
2024-09-01 22:00:47 +0530

    Update loader.c

--- loader.c
+++ loader.c
@@ -86,22 +86,3 @@ void load_and_run_elf(char** exe) {
   // 6. Call the "_start" method and print the value returned from the "_start"
     printf("User _start return value = %d\n",result);
 }
-int main(int argc, char** argv)
-{
-    if(argc != 2) {
-        printf("Usage: %s <ELF Executable> \n",argv[0]);
-        exit(1);
-  }
-  // 1. carry out necessary checks on the input ELF file
-    int fd = open(argv[1], O_RDONLY);
-    if (fd < 0) {
-        perror("Error: Failed to open ELF file");
-        exit(1);
-    }
-    close(fd);
-  // 2. passing it to the loader for carrying out the loading/execution
-    load_and_run_elf(argv);
-  // 3. invoke the cleanup routine inside the loader
-    loader_cleanup();
-    return 0;
-}
