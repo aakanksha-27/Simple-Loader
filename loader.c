fbcfccf060b90b480b337c50523bcf06aac67775
aakanksha-27 <163016647+aakanksha-27@users.noreply.github.com>
2024-09-01 18:47:22 +0530

    Update loader.c

--- loader.c
+++ loader.c
@@ -93,6 +93,12 @@ int main(int argc, char** argv)
         exit(1);
   }
   // 1. carry out necessary checks on the input ELF file
+    int fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        perror("Error: Failed to open ELF file");
+        exit(1);
+    }
+    close(fd);
   // 2. passing it to the loader for carrying out the loading/execution
     load_and_run_elf(argv);
   // 3. invoke the cleanup routine inside the loader
