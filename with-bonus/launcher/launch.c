4a56f804d0b8169510d9a85cf3e090298d537d87
aakanksha-27 <163016647+aakanksha-27@users.noreply.github.com>
2024-09-02 14:57:55 +0530

    Update launch.c

--- with-bonus/launcher/launch.c
+++ with-bonus/launcher/launch.c
@@ -1,12 +1,11 @@
 #include "loader.h"
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     if(argc != 2) {
         printf("Usage: %s <ELF Executable> \n",argv[0]);
         exit(1);
-  }
-  // 1. carry out necessary checks on the input ELF file
+    }
+// 1. carry out necessary checks on the input ELF file
     int fd = open(argv[1], O_RDONLY);
     if (fd < 0) {
         perror("Error: Failed to open ELF file");
@@ -14,9 +13,10 @@ int main(int argc, char** argv)
     }
     close(fd);
 
-  // 2. passing it to the loader for carrying out the loading/execution
+// 2. passing it to the loader for carrying out the loading/execution
     load_and_run_elf(argv);
-  // 3. invoke the cleanup routine inside the loader
+    
+// 3. invoke the cleanup routine inside the loader
     loader_cleanup();
     return 0;
 }
